6078e1c310ffa70b896a731b96b3a9615bbc74fa
Muhammad Aitisam Yaseen <geeker.one.afu.systems@gmail.com>
2024-06-14 16:50:00 -0700

    6th push

--- src/partials/Footer.jsx
+++ src/partials/Footer.jsx
@@ -1,9 +1,12 @@
 import '../styles/footer.css'
 
+import { Link } from 'react-router-dom'
+
 const Footer = () => {
     return (
         <footer>
             <p>© quarters {new Date().getFullYear()}</p>
+            <Link className="link" to="/verify">Temporary Link</Link>
             <p>help@quarters.com</p>
         </footer>
     )
